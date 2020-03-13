915385ef63669a74c7962608ccc74a6923ba0284
Brengi Maximo-Lopez <brengimaximolopez@gmail.com>
2020-03-13 11:03:23 -0500

    profile UI is up to bar, add Side NavBar, && message feed is rendering...... work on pushing new user messages to feed without having to refresh

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import { App } from "./react";
 import { store, history } from "./redux";
-import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
