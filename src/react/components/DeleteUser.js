7b1313ddc9b1147212ce2c2488030e78288410f1
Jordan Haagenson <jdhaagenson@gmail.com>
2020-03-18 12:52:58 -0700

    finished delete component

--- src/react/components/DeleteUser.js
+++ src/react/components/DeleteUser.js
@@ -1,5 +1,60 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { deleteUser } from "../../redux";
-import { Card, Image, Divider, Header } from "semantic-ui-react";
+import { Button, Modal, Header } from "semantic-ui-react";
+import {getState} from 'redux'
 
+class DeleteUser extends Component{
+  state = {
+    username: getState().auth.login.result.token,
+    modalOpen: false
+  };
+
+  handleDelete = e => {
+    deleteUser(this.state.username)
+  };
+
+  handleOpen = () => {
+    this.setState({modalOpen:true})
+  }
+
+  handleClose = () => {
+    this.setState({modalOpen:false})
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal
+          floated='right'
+          trigger={<Button icon="user delete" color='red' onClick={this.handleOpen}>Delete User</Button>}
+          open={this.state.modalOpen}
+          onClose={this.handleClose}
+          centered={true}>
+        >
+          <Header>Are you sure you want to delete your account?</Header>
+          <Button
+            basic
+            icon="ban"
+            color='red'
+            onClick={this.handleClose}
+          >Cancel</Button>
+          <Button
+            icon="checkmark"
+            color='red'
+            onClick={this.handleDelete}
+          >Confirm</Button>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  (state) => ({
+    result:getState().users.deleteUser.result,
+    loading: getState().users.deleteUser.loading,
+    error: getState().users.deleteUser.error
+  }),
+  { deleteUser }
+)(DeleteUser)
